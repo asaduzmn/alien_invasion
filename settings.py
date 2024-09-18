ff512f294cb380580d06a8e59757d13cb9889a9e
asaduzmn <asaduzmnpsl@gmail.com>
2024-09-19 00:12:39 +0600

    Add settings
    -separate settings value from alien_invasion

--- settings.py
+++ settings.py
@@ -3,4 +3,8 @@ class Settings():
     A class to store all settings for Alien Invasion.
     """
     def __init__(self) -> None:
-        """Initiali"""
+        """Initialize the game's settings"""
+        #screen settings
+        self.screen_width = 1080
+        self.screen_height = 600
+        self.bg_color = (230,230,230)
