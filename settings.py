636f9b47758d88799732fb9ba730a13039546767
asaduzmn <asaduzmnpsl@gmail.com>
2024-09-21 08:54:47 +0600

    Add right and left ability within display of alien

--- settings.py
+++ settings.py
@@ -21,3 +21,6 @@ class Settings():
 
         #alien settings
         self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        #fleet direction q represents right; -1 represents left.
+        self.fleet_direction = 1
